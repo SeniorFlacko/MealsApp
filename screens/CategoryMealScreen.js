e4babebf19a84f10489a214f926390135c0f2739
Jesus Cristian Aleman Fuentes <jesus.aleman@globant.com>
2020-10-16 18:09:50 -0500

    passing a property between screens

--- screens/CategoryMealScreen.js
+++ screens/CategoryMealScreen.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import {View, StyleSheet, Text, Button} from 'react-native';
+import {CATEGORIES} from '../data/dummy-data';
+
 const CategoryMealScreen = (props) => {
+  const catId = props.navigation.getParam('categoryId');
+  const selectedCategory = CATEGORIES.find((cat) => cat.id == catId);
+
   return (
     <View style={styles.screen}>
       <Text>CategoryMealScreen works!</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button
         title="Go to Meal Detail"
         onPress={() => {
