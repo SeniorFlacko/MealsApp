1cb3a989a9f5ba5401f790c36274b7f06ebc3852
Jesus Cristian Aleman Fuentes <jesus.aleman@globant.com>
2020-10-16 18:03:20 -0500

    wrapping a element for making it clickable and styling header

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,20 +1,45 @@
 import React from 'react';
-import {View, StyleSheet, Text, Button, FlatList} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Text,
+  Button,
+  FlatList,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
 import {CATEGORIES} from '../data/dummy-data';
-
-const renderGridItem = (itemData) => {
-  return (
-    <View style={styles.gridItem}>
-      <Text>{itemData.item.title}</Text>
-    </View>
-  );
-};
+import Colors from '../constants/Colors';
 
 const CategoriesScreen = (props) => {
+  const renderGridItem = (itemData) => {
+    return (
+      <TouchableOpacity
+        style={styles.gridItem}
+        onPress={() => {
+          props.navigation.navigate({routeName: 'CategoryMeals'});
+        }}>
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
   return (
     <FlatList numColumns={2} data={CATEGORIES} renderItem={renderGridItem} />
   );
 };
+
+CategoriesScreen['navigationOptions'] = () => {
+  return {
+    title: 'Meal Categories',
+    headerStyle: {
+      backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
+  };
+};
+
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
