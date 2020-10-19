233ec23ee98fa82f48ddb6bd8b08bd9a4b25039e
Jesus Cristian Aleman Fuentes <jesus.aleman@globant.com>
2020-10-19 15:38:13 -0500

    detail screen with scrollview and using map for a list

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,22 +1,37 @@
 import React from 'react';
-import {View, StyleSheet, Text, Button} from 'react-native';
+import {View, StyleSheet, Text, Button, ScrollView, Image} from 'react-native';
 import {MEALS} from '../data/dummy-data';
 import HeaderButton from '../components/HeaderButton';
+
+const ListItem = (props) => {
+  return (
+    <View style={styles.listItem}>
+      <Text>{props.children}</Text>
+    </View>
+  );
+};
 
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam('mealId');
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
   return (
-    <View style={styles.screen}>
-      <Text>{selectedMeal.title}</Text>
-      <Button
-        title="Go back"
-        onPress={() => {
-          props.navigation.popToTop();
-        }}
-      />
-    </View>
+    <ScrollView>
+      <Image source={{uri: selectedMeal.imageUrl}} style={styles.image} />
+      <View style={styles.details}>
+        <Text>{selectedMeal.duration}m</Text>
+        <Text>{selectedMeal.complexity.toUpperCase()}</Text>
+        <Text>{selectedMeal.affordability.toUpperCase()}</Text>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      {selectedMeal.ingredients.map((ingredient) => {
+        return <ListItem key={ingredient}>{ingredient}</ListItem>;
+      })}
+      <Text style={styles.title}>Steps</Text>
+      {selectedMeal.steps.map((step) => {
+        return <ListItem key={step}>{step}</ListItem>;
+      })}
+    </ScrollView>
   );
 };
 
@@ -35,6 +50,27 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  title: {
+    fontSize: 22,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  details: {
+    flexDirection: 'row',
+    padding: 15,
+    justifyContent: 'space-around',
+  },
+  listItem: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 10,
   },
 });
 export default MealDetailScreen;
