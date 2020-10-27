45409491a6f28d77c3a1fed397d72c6a494d3c9b
Jesus Cristian Aleman Fuentes <jesus.aleman@globant.com>
2020-10-27 17:18:02 -0600

    rendering text when favorites is empty

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import MealList from '../components/MealList';
-import {TouchableOpacity, StyleSheet} from 'react-native';
+import {TouchableOpacity, StyleSheet, Text, View} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {useSelector} from 'react-redux';
 
 const FavoritesScreen = (props) => {
   const favoriteMeals = useSelector((state) => state.meals.favoriteMeals);
   // const favMeals = avaliableMeals.filter((meal) => meal.id === 'm1' || meal.id === 'm2');
+
+  if (favoriteMeals.length === 0 || !favoriteMeals) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text>No favorites added, start adding some!!</Text>
+      </View>
+    );
+  }
+
   return <MealList listData={favoriteMeals} navigation={props.navigation} />;
 };
 
